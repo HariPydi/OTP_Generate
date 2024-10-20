b4b67ee6e524c51b91f13a8370e7b5f3e24ce37d
HariPydi <162025001+HariPydi@users.noreply.github.com>
2024-10-20 14:13:36 +0530

    hari

--- main.js
+++ main.js
@@ -21,16 +21,16 @@ toggle.addEventListener("click", () => {
     button.className = "btn";
     para.className = "p";
     // toggle.innerText = "Dark";
-    toggle.alt="Dark"
-    toggle.src="https://as2.ftcdn.net/v2/jpg/05/78/50/85/1000_F_578508518_lSf79cg5sF7MibT89n7JaNcOGWPw2w3t.jpg"
-
-  }
-  
-  else if (toggle.alt == "Dark") {
+    toggle.alt = "Dark";
+    toggle.src =
+      "https://th.bing.com/th/id/OIP.mbzKbnNJkNGrAidzO5abtwHaHa?rs=1&pid=ImgDetMain";
+  } else if (toggle.alt == "Dark") {
     div.className = "dark";
     button.className = "btn2";
     para.className = "p2";
     toggle.alt = "Light";
-    toggle.src="https://th.bing.com/th/id/OIP.mbzKbnNJkNGrAidzO5abtwHaHa?rs=1&pid=ImgDetMain"
+
+    toggle.src =
+      "https://as2.ftcdn.net/v2/jpg/05/78/50/85/1000_F_578508518_lSf79cg5sF7MibT89n7JaNcOGWPw2w3t.jpg";
   }
 });
